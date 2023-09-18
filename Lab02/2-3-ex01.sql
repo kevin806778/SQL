8af69e5fa8c1c6f417a241a856714999d3387cb8
kevin806778 <87964245+kevin806778@users.noreply.github.com>
2023-09-18 11:59:13 +0800

    Add files via upload

---
+++ Lab02/2-3-ex01.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Student
+(
+Stu_id CHAR(5),
+Stu_name NVARCHAR(10)Not Null,
+Stu_head CHAR(5)Not Null,
+PRIMARY KEY(Stu_id)
+)
---
+++ Lab02/2-4-1.sql
@@ -0,0 +1,4 @@
+use MyschoolDB
+INSERT INTO Department
+VALUES('D001','資工系','李春雄'),
+	  ('D002','資管系','李雄安')
