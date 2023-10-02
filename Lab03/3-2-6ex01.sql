4b7d1b9ebd7d271bd8056daab5598625f90bb9df
kevin806778 <87964245+kevin806778@users.noreply.github.com>
2023-10-02 10:24:47 +0800

    Create 3-2-6ex01.sql

---
+++ Lab03/3-2-6ex01.sql
@@ -0,0 +1,5 @@
+INSERT INTO Employee
+VALUES	('S0006','六和','D010')
+
+INSERT 陳述式與 FOREIGN KEY 條件約束 "FK__Employee__Em_cod__3A81B327" 衝突。衝突發生在資料庫 "MYSchoolDB"，資料表 "dbo.Comp_dep", column 'Comp_dep_code'。
+陳述式已經結束。
